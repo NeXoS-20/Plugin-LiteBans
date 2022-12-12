afcf405c06d888e1e7b1c51b38163178c9b3dc1d
MODcraft <admin@ncgs.top>
2022-12-12 12:14:34 +0000

    Chinses Translation

---
+++ resources/lang/zh-CN/messages.php
@@ -0,0 +1,34 @@
+<?php
+
+return [
+    'navigation' => [
+        'bans' => '封禁',
+        'mutes' => '禁言',
+        'kicks' => '踢出',
+        'warns' => '警告',
+        'search' => '搜索',
+    ],
+
+    'title' => '处罚',
+    'plugin_name' => 'Litebans / 处罚',
+    'history' => '历史',
+
+    'username' => '玩家',
+    'staff_ban' => '封禁者',
+    'staff_mute' => '禁言者',
+    'staff_kick' => '踢出者',
+    'staff_warn' => '警告者',
+    'staff_punish' => '处罚者',
+    'reason' => '原因',
+    'expires_at' => '失效于',
+    'no_punishments_found' => '未在数据库中查询到任何处罚数据.',
+
+    'active' => '生效中',
+    'finished' => '已结束',
+    'unbanned' => '已解除',
+    'expired' => '已过期',
+    'permanent' => '用就',
+
+    'given_punishments' => '处罚数据',
+    'staff' => '员工',
+];
